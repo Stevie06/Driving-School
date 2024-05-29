54423b2f8eb73e4937fe2d6727b4a846d260f1f1
Stevie06 <115479511+Stevie06@users.noreply.github.com>
2024-05-29 15:27:12 +0300

    First date
    First date

--- src/client.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://eoigrvfqdixpgpewqysu.supabase.co'
-const supabaseKey='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvaWdydmZxZGl4cGdwZXdxeXN1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYzODIyNjUsImV4cCI6MjAzMTk1ODI2NX0._l193FGq1yO7SAqgMHHwkYgrgG_ix-DuwQ-WBfCSeuY'   
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
-export default supabase
--- src/pages/index.js
+++
@@ -1,8 +0,0 @@
-import Login from './Login'
-import SignUp from './SignUp'
-import DashboardInst from './DashboardInst';
-import Studenti from './Studenti';
-import DashboardAdmin from './DashboardAdmin';
-import DashboardStud from './DashboardStud';
-
-export {Login,SignUp,DashboardInst,Studenti,DashboardAdmin,DashboardStud}
